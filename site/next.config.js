dca1d275feb4d363b23574d154f84abf606c9a2e
prot0y <shaunrives@gmail.com>
2026-06-08 19:56:18 -0400

    Update icon URL in next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   env: {
     name: 'STAR astro Workspaces',
     description: 'Personal source for Kasm supported workspaces.',
-    icon: 'https://prot0y.github.io/kasm-registry/1.0/moonlight.png',
+    icon: 'https://prot0y.github.io/kasm-registry/1.1/transparent-logo.png',
     listUrl: 'https://prot0y.github.io/kasm-registry/',
     contactUrl: 'https://github.com/prot0y/kasm-registry/issues',
   },
